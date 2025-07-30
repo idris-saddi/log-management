1a369555daef1d06b521c681beb760f78d042e81
idris <saddi.idris@gmal.com>
2025-07-30 19:42:36 +0100

    combine all docker compose

--- log-service/src/main/java/com/idris/log_service/LogServiceApplication.java
+++ log-service/src/main/java/com/idris/log_service/LogServiceApplication.java
@@ -2,12 +2,17 @@ package com.idris.log_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class LogServiceApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LogServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogServiceApplication.class, args);
+		logger.info("🚀 Hello from LogService – should appear in Graylog!");
 	}
 
 }
